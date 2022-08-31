5cc0ec7a0dd11dc9ffe8111d3ade1041ce03a43c
Zachary Gexler <zacharygexler@Zacharys-MacBook-Pro.local>
2022-08-31 08:11:59 -0400

    ejs working

---
+++ models/drinks.js
@@ -0,0 +1,43 @@
+
+const drinks = [
+    {
+      name: "cruddy mary",
+      price: 132,
+      image: "https://i.imgur.com/Va5iIw5",
+    },
+    {
+      name: "index on the beach",
+      price: 68,
+      image: "https://i.imgur.com/XV2aPa2",
+    },
+    {
+      name: "hack & coke",
+      price: 1,
+      image: "https://i.imgur.com/rLOXFRI",
+    },
+    {
+      name: "whiskey-value pair",
+      price: 11,
+      image: "https://i.imgur.com/a7rmkoS",
+    },
+    {
+      name: "404 horsemen",
+      price: 202,
+      image: "https://i.imgur.com/FLucOLr",
+    },
+    {
+      name: "if stateMint julep",
+      price: 2,
+      image: "https://i.imgur.com/yaoK0Dg",
+    },
+    {
+      name: "APIPA",
+      price: 43,
+      image: "https://i.imgur.com/qAhA7pi",
+    },
+    {
+      name: "node to joy",
+      price: 56,
+      image: "https://i.imgur.com/MbVdwZz",
+    },
+  ]
--- server.js
+++ server.js
@@ -6,14 +6,22 @@ const express = require("express");
 
 const app = express();
 
+const drinks = require("./models/drinks.js");
+
 //define routes
 
 // app.get("/", (req, res) => {
 //   res.send("ZACH GEXLER")
 // })
 
-app.get("/", (req, res) => {
-    res.send("Welcome To The Gitpub App")
+// app.get("/", (req, res) => {
+//     res.send("Welcome To The Gitpub App")
+//   })
+
+
+app.get("/drinks/", (req, res) => {
+    // res.send(drinks);
+    res.render("drinks_index.ejs");
   })
 
   app.listen(3000, function () {
