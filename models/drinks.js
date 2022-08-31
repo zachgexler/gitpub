11233c8bfc456602a9887c3c584c79a57a412594
Zachary Gexler <zacharygexler@Zacharys-MacBook-Pro.local>
2022-08-31 18:26:08 -0400

    fixed foler and file pathways and added food/drinks show

--- models/drinks.js
+++ models/drinks.js
@@ -1,50 +1,44 @@
-
 const drinks = [
-    {
-      name: "cruddy mary",
-      price: 132,
-      image: "https://i.imgur.com/Va5iIw5",
-    },
-    {
-      name: "index on the beach",
-      price: 68,
-      image: "https://i.imgur.com/XV2aPa2",
-    },
-    {
-      name: "hack & coke",
-      price: 1,
-      image: "https://i.imgur.com/rLOXFRI",
-    },
-    {
-      name: "whiskey-value pair",
-      price: 11,
-      image: "https://i.imgur.com/a7rmkoS",
-    },
-    {
-      name: "404 horsemen",
-      price: 202,
-      image: "https://i.imgur.com/FLucOLr",
-    },
-    {
-      name: "if stateMint julep",
-      price: 2,
-      image: "https://i.imgur.com/yaoK0Dg",
-    },
-    {
-      name: "APIPA",
-      price: 43,
-      image: "https://i.imgur.com/qAhA7pi",
-    },
-    {
-      name: "node to joy",
-      price: 56,
-      image: "https://i.imgur.com/MbVdwZz",
-    },
-  ]
-  
-  <% for (let i =0; i <allDrinks.length; i++) { %>
-    <li>
-        <a href="drinks/<%=i%>"><%=allDrinks[i].name.toUpperCase() %></a>
-    </li>
-
-  }
+  {
+    name: "cruddy mary",
+    price: 132,
+    image: "https://i.imgur.com/Va5iIw5",
+  },
+  {
+    name: "index on the beach",
+    price: 68,
+    image: "https://i.imgur.com/XV2aPa2",
+  },
+  {
+    name: "hack & coke",
+    price: 1,
+    image: "https://i.imgur.com/rLOXFRI",
+  },
+  {
+    name: "whiskey-value pair",
+    price: 11,
+    image: "https://i.imgur.com/a7rmkoS",
+  },
+  {
+    name: "404 horsemen",
+    price: 202,
+    image: "https://i.imgur.com/FLucOLr",
+  },
+  {
+    name: "if stateMint julep",
+    price: 2,
+    image: "https://i.imgur.com/yaoK0Dg",
+  },
+  {
+    name: "APIPA",
+    price: 43,
+    image: "https://i.imgur.com/qAhA7pi",
+  },
+  {
+    name: "node to joy",
+    price: 56,
+    image: "https://i.imgur.com/MbVdwZz",
+  },
+]
+console.log(module)
+module.exports = drinks
--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-require("express");
 
-console.log(require("express"))
 
 const express = require("express");
-
+const drinks = require("./models/drinks.js")
+const food = require("./models/food.js")
+// INITIALIZE EXPRESS APP
 const app = express();
-
-const drinks = require("./models/drinks.js");
-
-//define routes
-
-// app.get("/", (req, res) => {
-//   res.send("ZACH GEXLER")
-// })
-
-// app.get("/", (req, res) => {
-//     res.send("Welcome To The Gitpub App")
-//   })
-
+const port = 3000;
 
 app.get("/drinks/", (req, res) => {
-    // res.send(drinks);
-    res.render("drinks_index.ejs");
-  })
-
-  app.listen(3000, function () {
-    console.log("express is listening for requests from the browser")
+res.render("drinks_index.ejs", {
+  allDrinks: drinks,
 })
+});
+
+app.get("/food/", (req, res) => {
+  res.render("food_index.ejs", {
+    allFood: food,
+  })
+});
+
+app.get("/drinks/:indexOfDrinksArray", (req, res) => {
+  res.render('drinks_show.ejs', {
+    drink: drinks[req.params.indexOfDrinksArray],
+  })
+});
+
+app.get("/food/:indexOfFoodArray", (req, res) => {
+  res.render('food_show.ejs', {
+    food: food[req.params.indexOfFoodArray],
+  })
+});
+
+// TELL OUR APP TO LISTEN ON PORT...
+app.listen(port, () => {
+  console.log(`listening on port `, port)
+});
